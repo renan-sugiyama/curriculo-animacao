616cf9ad57abec8f71af7a42c2b880304eda9743
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-06 05:04:09 -0300

    objetos de fundo, animcaoes, telas de fundo closes #2 closes #3

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -65,266 +65,225 @@ $(function () { // wait for document ready
                             .addTo(controller);
     }
 
-    new ScrollMagic.Scene({
-        duration: 700,    // the scene should last for a scroll distance of 100px
-        offset: 10000    // start this scene after scrolling for 50px
-        })
-        .setTween(TweenMax.to(".contato", 1, {className: "+=show-contato"}))
-        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
-        .removeClassToggle(true)
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        duration: 700,    // the scene should last for a scroll distance of 100px
-        offset: 5400    // start this scene after scrolling for 50px
-        })
-        .setTween(TweenMax.to(".tela-escura-efeito", 1, {className: "+=tela-escura-efeito-hide"}))
-        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
-        .removeClassToggle(true)
-        .addTo(controller);
+    function toggleClass (offset, duration, target, className) {
+        new ScrollMagic.Scene({
+            duration: duration,    // the scene should last for a scroll distance of 100px
+            offset: offset    // start this scene after scrolling for 50px
+            })
+            .setTween(TweenMax.to(target, 1, {className: "+=" + className}))
+            .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+            .removeClassToggle(true)
+            .addTo(controller);
+    }
     
+
+
+
+
+
+
+    //teste email desenho
+    var flightpath = {
+        entry : {
+            curviness: 1.25,
+            autoRotate: true,
+            values: [
+                {x:0, y:0},
+                {x:$(window).width()*1.2 - $(window).width(), y:0}, 
+                {x:$(window).width()*1.5 - $(window).width(), y:-80}, 
+                {x:$(window).width() - $(window).width()/1.2, y:0},
+                {x:0, y:$(window).height()-$(window).height()/2},
+                {x:$(window).width() - $(window).width()/1.1, y:$(window).height()/1.5-$(window).height()},
+                {x:$(window).width() - $(window).width()/2, y:-$(window).height()-800},
+                {x:$(window).width(), y:-$(window).height()-200}
+            ]
+        }
+    };
+    
+
+    
+    // create tween
+    var emailtween = new TimelineMax()
+        .add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}));
+
+    // build scene
+    var scene = new ScrollMagic.Scene({
+        duration: 2000,    // the scene should last for a scroll distance of 100px
+        offset: 9000    // start this scene after scrolling for 50px
+        })
+                    .setPin("#target")
+                    .setTween(emailtween)
+                    .addIndicators('teste') // add indicators (requires plugin)
+                    .addTo(controller);
+                    
+
+
+
+// anima personagem
+                   
+    var imagesRun = [
+        "./assets/imgs/sprites/personagem/Run1.png",
+        "./assets/imgs/sprites/personagem/Run2.png",
+        "./assets/imgs/sprites/personagem/Run3.png",
+        "./assets/imgs/sprites/personagem/Run4.png",
+        "./assets/imgs/sprites/personagem/Run5.png",
+        "./assets/imgs/sprites/personagem/Run6.png",
+        "./assets/imgs/sprites/personagem/Run7.png",
+        "./assets/imgs/sprites/personagem/Run8.png",
+        "./assets/imgs/sprites/personagem/Run9.png",
+        "./assets/imgs/sprites/personagem/Run10.png",
+        "./assets/imgs/sprites/personagem/Run11.png",
+        "./assets/imgs/sprites/personagem/Run12.png",
+        "./assets/imgs/sprites/personagem/Run13.png",
+        "./assets/imgs/sprites/personagem/Run14.png",
+        "./assets/imgs/sprites/personagem/Run15.png",
+        
+    ];
+
+    var imagesJump = [
+        "./assets/imgs/sprites/personagem/Jump1.png",
+        "./assets/imgs/sprites/personagem/Jump2.png",
+        "./assets/imgs/sprites/personagem/Jump3.png",
+        "./assets/imgs/sprites/personagem/Jump4.png",
+        "./assets/imgs/sprites/personagem/Jump5.png",
+        "./assets/imgs/sprites/personagem/Jump6.png",
+        "./assets/imgs/sprites/personagem/Jump7.png",
+        "./assets/imgs/sprites/personagem/Jump8.png",
+        "./assets/imgs/sprites/personagem/Jump9.png",
+        "./assets/imgs/sprites/personagem/Jump10.png",
+        "./assets/imgs/sprites/personagem/Jump11.png",
+        "./assets/imgs/sprites/personagem/Jump12.png",
+        "./assets/imgs/sprites/personagem/Jump13.png",
+        "./assets/imgs/sprites/personagem/Jump14.png",
+        "./assets/imgs/sprites/personagem/Jump15.png",
+        
+    ];
+            
+    // TweenMax can tween any property of any object. We use this object to cycle through the array
+    var obj = {curImg: 0};
+
+    // create tween
+    var personagemtween = TweenMax.to(obj, 0.5,
+        {
+            curImg: imagesRun.length - 1,	// animate propery curImg to number of imagesRun
+            roundProps: "curImg",				// only integers so it can be used as an array index
+            repeat: 1,									// repeat 3 times
+            immediateRender: true,			// load first image automatically
+            ease: Linear.easeNone,			// show every image the same ammount of time
+            onUpdate: function () {
+                $("#personagem").attr("src", imagesRun[obj.curImg]); // set the image source
+            }
+        }
+    );
+
+
+
+
+
+
+    
+            
+    // TweenMax can tween any property of any object. We use this object to cycle through the array
+    var obj2 = {curImg: 0};
+
+    // create tween
+    var personagemJumptween = TweenMax.to(obj2, 0.5,
+        {
+            curImg: imagesJump.length - 1,	// animate propery curImg to number of imagesJump
+            roundProps: "curImg",				// only integers so it can be used as an array index
+            repeat: 1,									// repeat 3 times
+            immediateRender: true,			// load first image automatically
+            ease: Linear.easeNone,			// show every image the same ammount of time
+            onUpdate: function () {
+                $("#personagem").attr("src", imagesJump[obj2.curImg]); // set the image source
+            }
+        }
+    );
+
+
+
+   
+    // build scene
+    function animaSprite (offset, duration){
+        new ScrollMagic.Scene({
+            offset: offset, 
+            duration: duration  
+        })
+        .setTween(personagemtween)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    }
+
+    function animaJumpSprite (offset, duration){
+        new ScrollMagic.Scene({
+            offset: offset, 
+            duration: duration  
+        })
+        .setTween(personagemJumptween)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    }
+
+
+
     deslocarTela(100, 5000, 0, '-200vw', 0, 0)
     deslocarTela(5000, 1000, '-200vw', '-200vw', 0, '-100vh')
     deslocarTela(6700, 4000, '-200vw', '-400vw', '-100vh', '-100vh')
-
-
-        
-
-        //anda para a direita
-        //build scene
-        function esconder (offset) {
-            new ScrollMagic.Scene({
-                duration: 100,    // the scene should last for a scroll distance of 100px
-                offset: offset    // start this scene after scrolling for 50px
-                })
-                .setTween(TweenMax.to(".personagem", 1, {className: "+=hidePersonagem"}))
-                .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
-                .removeClassToggle(true)
-                .addTo(controller);
-        }
-
-
-        
-
-
-        //movHorizontal(10, '10%', '80%')
-
-        movVertical(1200, '70%', '50%')
-        movVertical(1400, '50%', '70%')
-        
-        movVertical(1700, '70%', '50%')
-        movVertical(1900, '50%', '70%')
-
-        movVertical(2200, '70%', '50%')
-        movVertical(2400, '50%', '70%')
-
-        movVertical(3700, '70%', '50%')
-        movVertical(4400, '50%', '70%')
-                    
-        //movHorizontal(2600, '40%', '70%')
-        
-        movVertical(6700, '70%', '60%')
-        
-        movVertical(7000, '60%', '40%')
-
-        movVertical(7300, '40%', '20%')
-
-        movVertical(7600, '20%', '10%')
-
-        movHorizontal(7800, '10%', '50%')
-        movVertical(8000, '10%', '70%')
-
-        esconder(8400)
-        //movHorizontal(5200, '80%', '10%')
-
-        // pula(200)
-        // document.querySelector('.personagem').classList.remove('personagem-up')
-        // desce(260)
-        // document.querySelector('.personagem').classList.remove('personagem-down')
-
-        // pula(350)
-        // desce(410)
-
-        // pula(500)
-        // desce(560)
-
-
-
-
-        //teste email desenho
-        var flightpath = {
-			entry : {
-				curviness: 1.25,
-				autoRotate: true,
-                values: [
-                    {x:0, y:0},
-                    {x:$(window).width()*1.2 - $(window).width(), y:0}, 
-                    {x:$(window).width()*1.5 - $(window).width(), y:-80}, 
-                    {x:$(window).width() - $(window).width()/1.2, y:0},
-                    {x:0, y:$(window).height()-$(window).height()/2},
-                    {x:$(window).width() - $(window).width()/1.1, y:$(window).height()/1.5-$(window).height()},
-                    {x:$(window).width() - $(window).width()/2, y:-$(window).height()-800},
-                    {x:$(window).width(), y:-$(window).height()-200}
-                ]
-			}
-        };
-        
-
-
-
-        
-            new ScrollMagic.Scene({
-                duration: 100,    // the scene should last for a scroll distance of 100px
-                offset: 9000    // start this scene after scrolling for 50px
-                })
-                .setTween(TweenMax.to("#target", 1, {className: "+=showTarget"}))
-                .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
-                .removeClassToggle(true)
-                .addTo(controller);
-        
-
-
-		
-		// create tween
-		var emailtween = new TimelineMax()
-			.add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}));
-
-		// build scene
-		var scene = new ScrollMagic.Scene({
-            duration: 2000,    // the scene should last for a scroll distance of 100px
-            offset: 9000    // start this scene after scrolling for 50px
-            })
-						.setPin("#target")
-						.setTween(emailtween)
-						.addIndicators('teste') // add indicators (requires plugin)
-                        .addTo(controller);
-                        
-
+    toggleClass(5400, 700, ".tela-escura-efeito", 'tela-escura-efeito-hide')
+    //toggleClass(20000, 700, ".contato", 'show-contato')
+    toggleClass(9000, 100, "#target", 'showTarget')
     
+    movVertical(1200, '70%', '50%')
+    movVertical(1400, '50%', '70%')
+    toggleClass(1400, 100, ".parte1 .tijolo1", 'html5Skill')
+    
+    movVertical(1700, '70%', '50%')
+    movVertical(1900, '50%', '70%')
+    toggleClass(1900, 100, ".parte1 .tijolo2", 'css3Skill')
 
-    // anima personagem
-	                   
-        var imagesRun = [
-            "./assets/imgs/sprites/personagem/Run1.png",
-            "./assets/imgs/sprites/personagem/Run2.png",
-            "./assets/imgs/sprites/personagem/Run3.png",
-            "./assets/imgs/sprites/personagem/Run4.png",
-            "./assets/imgs/sprites/personagem/Run5.png",
-            "./assets/imgs/sprites/personagem/Run6.png",
-            "./assets/imgs/sprites/personagem/Run7.png",
-            "./assets/imgs/sprites/personagem/Run8.png",
-            "./assets/imgs/sprites/personagem/Run9.png",
-            "./assets/imgs/sprites/personagem/Run10.png",
-            "./assets/imgs/sprites/personagem/Run11.png",
-            "./assets/imgs/sprites/personagem/Run12.png",
-            "./assets/imgs/sprites/personagem/Run13.png",
-            "./assets/imgs/sprites/personagem/Run14.png",
-            "./assets/imgs/sprites/personagem/Run15.png",
-            
-        ];
+    movVertical(2200, '70%', '50%')
+    movVertical(2400, '50%', '70%')
+    toggleClass(2400, 100, ".parte1 .tijolo3", 'javascriptSkill')
 
-        var imagesJump = [
-            "./assets/imgs/sprites/personagem/Jump1.png",
-            "./assets/imgs/sprites/personagem/Jump2.png",
-            "./assets/imgs/sprites/personagem/Jump3.png",
-            "./assets/imgs/sprites/personagem/Jump4.png",
-            "./assets/imgs/sprites/personagem/Jump5.png",
-            "./assets/imgs/sprites/personagem/Jump6.png",
-            "./assets/imgs/sprites/personagem/Jump7.png",
-            "./assets/imgs/sprites/personagem/Jump8.png",
-            "./assets/imgs/sprites/personagem/Jump9.png",
-            "./assets/imgs/sprites/personagem/Jump10.png",
-            "./assets/imgs/sprites/personagem/Jump11.png",
-            "./assets/imgs/sprites/personagem/Jump12.png",
-            "./assets/imgs/sprites/personagem/Jump13.png",
-            "./assets/imgs/sprites/personagem/Jump14.png",
-            "./assets/imgs/sprites/personagem/Jump15.png",
-            
-        ];
+    movVertical(3700, '70%', '50%')
+    movVertical(4400, '50%', '70%')
                 
-        // TweenMax can tween any property of any object. We use this object to cycle through the array
-        var obj = {curImg: 0};
     
-        // create tween
-        var personagemtween = TweenMax.to(obj, 0.5,
-            {
-                curImg: imagesRun.length - 1,	// animate propery curImg to number of imagesRun
-                roundProps: "curImg",				// only integers so it can be used as an array index
-                repeat: 1,									// repeat 3 times
-                immediateRender: true,			// load first image automatically
-                ease: Linear.easeNone,			// show every image the same ammount of time
-                onUpdate: function () {
-                    $("#personagem").attr("src", imagesRun[obj.curImg]); // set the image source
-                }
-            }
-        );
-
-
-
-
-
-
-        
-                
-        // TweenMax can tween any property of any object. We use this object to cycle through the array
-        var obj2 = {curImg: 0};
+    movVertical(6700, '70%', '60%')
+    toggleClass(2400, 100, ".parte1 .tijolo3", 'javascriptSkill')
     
-        // create tween
-        var personagemJumptween = TweenMax.to(obj2, 0.5,
-            {
-                curImg: imagesJump.length - 1,	// animate propery curImg to number of imagesJump
-                roundProps: "curImg",				// only integers so it can be used as an array index
-                repeat: 1,									// repeat 3 times
-                immediateRender: true,			// load first image automatically
-                ease: Linear.easeNone,			// show every image the same ammount of time
-                onUpdate: function () {
-                    $("#personagem").attr("src", imagesJump[obj2.curImg]); // set the image source
-                }
-            }
-        );
+    movVertical(7000, '60%', '40%')
+    toggleClass(7050, 200, ".parte2 .tijolo1", 'frameSkill-hide')
 
+    movVertical(7300, '40%', '20%')
+    toggleClass(7350, 200, ".parte2 .tijolo2", 'frameSkill-hide')
 
+    movVertical(7600, '20%', '10%')
+    toggleClass(7650, 200, ".parte2 .tijolo3", 'frameSkill-hide')
 
-       
-        // build scene
-        function animaSprite (offset, duration){
-            new ScrollMagic.Scene({
-                offset: offset, 
-                duration: duration  
-            })
-            .setTween(personagemtween)
-            .addIndicators() // add indicators (requires plugin)
-            .addTo(controller);
-        }
+    movHorizontal(7800, '10%', '50%')
+    movVertical(8000, '10%', '70%')
 
-        function animaJumpSprite (offset, duration){
-            new ScrollMagic.Scene({
-                offset: offset, 
-                duration: duration  
-            })
-            .setTween(personagemJumptween)
-            .addIndicators() // add indicators (requires plugin)
-            .addTo(controller);
-        }
+    toggleClass(8400, 100, ".personagem", 'hidePersonagem')
 
-        animaSprite(0, 500)
-        animaSprite(500, 500)
-        animaSprite(1000, 200)
-        animaJumpSprite(1200, 400)
-        animaSprite(1600, 100)
-        animaJumpSprite(1700, 400)
-        animaSprite(2100, 100)
-        animaJumpSprite(2200, 400)
-        animaSprite(2500, 500)
-        animaSprite(3000, 500)
-        animaSprite(3500, 200)
-        animaJumpSprite(3700, 1300)
-        animaSprite(6500, 200)
-        animaJumpSprite(6700, 250)
-        animaJumpSprite(7000, 250)
-        animaJumpSprite(7300, 250)
-        animaJumpSprite(7600, 400)
-        animaSprite(8200, 500)
+    animaSprite(0, 500)
+    animaSprite(500, 500)
+    animaSprite(1000, 200)
+    animaJumpSprite(1200, 400)
+    animaSprite(1600, 100)
+    animaJumpSprite(1700, 400)
+    animaSprite(2100, 100)
+    animaJumpSprite(2200, 400)
+    animaSprite(2500, 500)
+    animaSprite(3000, 500)
+    animaSprite(3500, 200)
+    animaJumpSprite(3700, 1300)
+    animaSprite(6500, 200)
+    animaJumpSprite(6700, 250)
+    animaJumpSprite(7000, 250)
+    animaJumpSprite(7300, 250)
+    animaJumpSprite(7600, 400)
+    animaSprite(8200, 500)
 
 
         
