7b1740acb27096f52fb96344da14f9dbb02c1a1c
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-07 20:05:21 -0300

    ajuste para nao fazer o reload quando dar focus em inputs e textarea

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ var width = $(window).width(), height = $(window).height();
 
 window.addEventListener("resize", ()=>{
     if($(window).width() != width || $(window).height() != height){
-        //Do something
+        if(!document.querySelector('#name').hasFocus() || !document.querySelector('#email').hasFocus() || !document.querySelector('#message').hasFocus())
         location.reload();
         }
   });
