38b38f3b96d11bb5cc21f528ae592d540ac83051
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-07 19:21:01 -0300

    correcao de resize em mobile

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,9 @@
+
+var width = $(window).width(), height = $(window).height();
+
 window.addEventListener("resize", ()=>{
-    location.reload();
+    if($(window).width() != width || $(window).height() != height){
+        //Do something
+        location.reload();
+        }
   });
